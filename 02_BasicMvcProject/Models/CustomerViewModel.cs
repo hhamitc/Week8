b6d44ca292cf38d4b25666b83504c1f52ad59400
Hamit CANPOLAT <104701563+hhamitc@users.noreply.github.com>
2024-11-19 15:05:32 +0300

    Delete 02_BasicMvcProject directory

--- 02_BasicMvcProject/Models/CustomerViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _02_BasicMvcProject.Models
-{
-    public class CustomerViewModel
-    {
-        public Customer Customer { get; set; }
-        public string WelcomeMessage { get; set; } // Ekstra bir veri
-
-    }
-}
--- 02_BasicMvcProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _02_BasicMvcProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
